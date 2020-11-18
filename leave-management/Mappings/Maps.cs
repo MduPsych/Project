1246526742610d56816a1c6a7060cf780a66acb5
MduPsych <26905647@student.g.nwu.ac.za>
2020-11-18 22:00:01 +0200

    Changes

---
+++ leave-management/Mappings/Maps.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using leave_management.Data;
+using leave_management.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management.Mappings
+{
+    public class Maps : Profile
+    {
+        public  Maps()
+        {
+            CreateMap<LeaveType, DetailsLeaveTypeVM>().ReverseMap();
+            CreateMap<LeaveType, DetailsLeaveTypeVM>().ReverseMap();
+            CreateMap<LeaveHistory, LeaveAllocationVM>().ReverseMap();
+            CreateMap<LeaveAllocation, LeaveAllocation>().ReverseMap();
+            CreateMap<Employee, EmployeeVM>().ReverseMap();
+
+        }
+    }
+}
